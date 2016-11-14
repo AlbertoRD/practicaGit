a6619daae3461a40a3ed719851786c392e559ed8
AlbertoRD <alberto.rodriguez5493@gmail.com>
2016-11-14 18:23:01 +0100

    test commit

--- src/practicaGit/GitAux.java
+++ src/practicaGit/GitAux.java
@@ -5,10 +5,16 @@ public class GitAux {
 	
 	public void metodoAlumno1(){
 		System.out.println("metodo 1");
+		
+		System.out.println("modificado por alum 1"); // insertado por alum1
+
+		
 	}
 	
 	public void metodoComunitario(){
 		//comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
+		
+		System.out.println("alumn 1 inserta esta sentencia");
 	}
 }
