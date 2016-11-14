423c016ebc343cb0aaca16a52ac5665c04ffc83c
jaime2306 <jaime23t@outlook.com>
2016-11-14 18:25:39 +0100

    alumno 2 commit

--- src/practicaGit/GitAux.java
+++ src/practicaGit/GitAux.java
@@ -3,12 +3,13 @@ package practicaGit;
 public class GitAux {
 
 	
-	public void metodoAlumno1(){
-		System.out.println("metodo 1");
+	public void metodoAlumno2(){
+		System.out.println("metodo 2");
 	}
 	
 	public void metodoComunitario(){
 		//comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
+		System.out.println("alumno 2"); //modif por alumno 2
 	}
 }
